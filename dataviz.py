695f7136a9d2b56814a8e8fc63b84b4737015a90
EdwinS78 <80060721+EdwinS78@users.noreply.github.com>
2023-12-11 17:13:52 +0100

    Update dataviz.py

--- dataviz.py
+++ dataviz.py
@@ -238,16 +238,3 @@ if granularity == 'Par heure':
 st.altair_chart(chart, use_container_width=True)
 
 
-from streamlit_echarts import st_echarts
-
-options = {
-    "xAxis": {
-        "type": "category",
-        "data": ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-    },
-    "yAxis": {"type": "value"},
-    "series": [
-        {"data": [820, 932, 901, 934, 1290, 1330, 1320], "type": "line"}
-    ],
-}
-st_echarts(options=options)
